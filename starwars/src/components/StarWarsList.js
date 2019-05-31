57e6a14fa58b625ee6717d2b69f9266f1314c86c
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-05-31 12:06:16 +0100

    implement pagination

--- starwars/src/components/StarWarsList.js
+++ starwars/src/components/StarWarsList.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import StarWars from './StarWars';
+import React from "react";
+import StarWars from "./StarWars";
+import StarWarsButton from './StarWarsButton';
 
-
-function StarWarsList({charactersArr}) {
-    console.log(charactersArr);
-return charactersArr.map(character => {
-    return <StarWars characters={character} />
-})
+function StarWarsList({ charactersArr, previousButton, nextButton }) {
+  const result = charactersArr.map(character => {
+    return <StarWars characters={character} />;
+  });
+return(
+   <div>
+       {result}
+       <StarWarsButton previousButton={previousButton} nextButton={nextButton} />
+   </div>
+)
 }
 
 export default StarWarsList;
